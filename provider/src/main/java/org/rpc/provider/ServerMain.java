19aa06f5895e329eeb827178b7550c6a5b249af9
fzl_angle <fzl_angle@163.com>
2019-06-02 23:44:47 +0800

    init

---
+++ provider/src/main/java/org/rpc/provider/ServerMain.java
@@ -0,0 +1,15 @@
+package org.rpc.provider;
+
+/**
+ * @author zhaoliang.fu
+ * @version V1.0
+ * @Package PACKAGE_NAME
+ * @Description:
+ * @date 2019/6/2 23:35
+ */
+public class ServerMain {
+
+    public static void main(String[] args) {
+
+    }
+}
