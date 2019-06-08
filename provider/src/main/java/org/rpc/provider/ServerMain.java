7c6a3690cd9ab6553da1cdf96ee0d59f6a032922
fuzhaoliang <fuzhaoliang@111.com.cn>
2019-06-08 16:40:03 +0800

    update

--- provider/src/main/java/org/rpc/provider/ServerMain.java
+++ provider/src/main/java/org/rpc/provider/ServerMain.java
@@ -1,5 +1,12 @@
 package org.rpc.provider;
 
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import org.rpc.provider.handler.ServerInitHandler;
+
 /**
  * @author zhaoliang.fu
  * @version V1.0
@@ -10,6 +17,15 @@ package org.rpc.provider;
 public class ServerMain {
 
     public static void main(String[] args) {
-
+        EventLoopGroup bossLoopGroup=new NioEventLoopGroup();
+        EventLoopGroup workLoopGroup=new NioEventLoopGroup();
+        ServerBootstrap serverBootstrap=new ServerBootstrap();
+        serverBootstrap.group(bossLoopGroup,workLoopGroup)
+                .childOption(ChannelOption.SO_KEEPALIVE,true)
+                .childOption(ChannelOption.TCP_NODELAY,true)
+                .option(ChannelOption.SO_BACKLOG, 1024)
+                .channel(NioServerSocketChannel.class)
+                .childHandler(new ServerInitHandler())
+                .bind(8760);
     }
 }
---
+++ provider/src/main/java/org/rpc/provider/handler/ServerInitHandler.java
@@ -0,0 +1,19 @@
+package org.rpc.provider.handler;
+
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.handler.timeout.IdleStateHandler;
+
+/**
+ * @version V1.0
+ * @ClassName: org.rpc.provider.handler.ServerInitHandler
+ * @Description:
+ * @author: fuzhaoliang
+ * @date: 2019/6/3 15:51
+ */
+public class ServerInitHandler extends ChannelInitializer<NioSocketChannel> {
+    @Override
+    protected void initChannel(NioSocketChannel ch) throws Exception {
+//        ch.pipeline().addLast(new IdleStateHandler())
+    }
+}
